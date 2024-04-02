7b78356c67ffe5a96d9bd0d67bd4f826ea4c4dfa
linh18nd <trinhhoailinh.dev@gmail.com>
2024-04-02 11:57:23 +0700

    change lab 4

--- bt4/main.c
+++ bt4/main.c
@@ -5,64 +5,58 @@
 // Khai báo semaphore và biến global
 sem_t semaphore_p1, semaphore_p2, semaphore_p3;
 
-// Hàm in và release semaphore
-void print_and_release(sem_t *sem, char *str) {
-    printf("%s ", str);
-    sem_post(sem);
-}
-
 // Tiến trình P1
 void *p1(void *arg) {
     sem_wait(&semaphore_p1);
-    print_and_release(&semaphore_p2, "R");
+    printf("R ");
+    sem_post(&semaphore_p2);
     sem_wait(&semaphore_p1);
-    print_and_release(&semaphore_p1, "OK");
+    
+    printf("OK ");
     pthread_exit(NULL);
 }
 
 // Tiến trình P2
 void *p2(void *arg) {
     sem_wait(&semaphore_p2);
-    print_and_release(&semaphore_p3, "I");
+    printf("I ");
+    sem_post(&semaphore_p3);
     sem_wait(&semaphore_p2);
-    print_and_release(&semaphore_p2, "OK");
+    printf("OK ");
+    sem_post(&semaphore_p1);
     pthread_exit(NULL);
 }
 
 // Tiến trình P3
 void *p3(void *arg) {
     sem_wait(&semaphore_p3);
-    print_and_release(&semaphore_p3, "O");
-    sem_post(&semaphore_p1);
-    sem_post(&semaphore_p1);
-    print_and_release(&semaphore_p1, "OK");
+    printf("O ");
+    printf("OK ");
+    sem_post(&semaphore_p2);
     pthread_exit(NULL);
 }
 
 int main() {
     pthread_t tid1, tid2, tid3;
-
+    printf("---Start---\n");
     // Khởi tạo semaphore
     sem_init(&semaphore_p1, 0, 1);
     sem_init(&semaphore_p2, 0, 0);
     sem_init(&semaphore_p3, 0, 0);
-
     // Tạo các tiến trình
     pthread_create(&tid1, NULL, p1, NULL);
     pthread_create(&tid2, NULL, p2, NULL);
     pthread_create(&tid3, NULL, p3, NULL);
-
     // Chờ tiến trình hoàn thành
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
     pthread_join(tid3, NULL);
-
     // Hủy semaphore
     sem_destroy(&semaphore_p1);
     sem_destroy(&semaphore_p2);
     sem_destroy(&semaphore_p3);
 
-    printf("\nFinished.\n");
+    printf("\n---Finished.---\n");
 
     return 0;
 }
